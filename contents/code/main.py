70ea304e272a1916925051df319f01343e22d799
Justin Riley <justin.t.riley@gmail.com>
2009-07-08 01:42:13 -0400

    Initial import of AWS DataEngine for Plasma

---
+++ contents/code/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from PyQt4.QtCore import *
+from PyKDE4.kdecore import *
+from PyKDE4 import plasmascript
+
+import starcluster
+
+class AWSEngine(plasmascript.DataEngine):
+    def __init__(self,parent,args=None):
+        plasmascript.DataEngine.__init__(self,parent)
+
+    def init(self):
+        self.setMinimumPollingInterval(20000)
+
+    def sources(self):
+        sources = ["instances", "images"]
+        return sources
+
+    def sourceRequestEvent(self, name):
+        return self.updateSourceEvent(name)
+
+    def updateSourceEvent(self, name):
+        if name == "images":
+            images = starcluster.ec2utils.get_registered_images()
+            for image_name in images:
+                image = images[image_name]
+                image = dict(zip(image.keys(), [QVariant(val) for val in image.values()])
+                self.setData(name, image_name, QVariant(image))
+        return True
+
+def CreateDataEngine(parent):
+    return AWSEngine(parent)
